999f0d07f48bfcea5eef650e0e5ef003c49a1c09
Timmy Zhu <nattofriends@gmail.com>
2014-08-13 21:44:55 -0700

    Use 2to3

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
-setup(name='rnc2rng',
-      version='1.0',
-      packages=['rnc2rng'],
-      )
+
+setup(
+    name='rnc2rng',
+    version='1.0',
+    packages=['rnc2rng'],
+    use_2to3 = True,
+)
