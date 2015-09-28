8d2026b5939c776b0581175f3a4b4369d1dfcceb
Dirkjan Ochtman <dirkjan@ochtman.nl>
2015-09-28 17:47:41 +0200

    Wrap command-line script code in a function

--- rnc2rng/__main__.py
+++ rnc2rng/__main__.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 import sys
 from rnctree import make_nodetree, token_list
-if len(sys.argv) > 1:
-    tokens = token_list(open(sys.argv[1]).read())
-else:
-    tokens = token_list(sys.stdin.read())
-root = make_nodetree(tokens)
-if len(sys.argv) > 2:
-    open(sys.argv[2], 'w').write(root.toxml())
-else:
-    print root.toxml()
+
+def main():
+
+    args = sys.argv[1:]
+    if len(args) > 0:
+        tokens = token_list(open(args[0]).read())
+    else:
+        tokens = token_list(sys.stdin.read())
+
+    root = make_nodetree(tokens)
+    if len(args) > 1:
+        open(sys.argv[2], 'w').write(root.toxml())
+    else:
+        print root.toxml()
+
+if __name__ == '__main__':
+    main()
