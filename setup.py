599662ae3e9edb6524901587476dc2ffa516e9c3
Dirkjan Ochtman <dirkjan@ochtman.nl>
2015-09-28 09:13:22 +0200

    Add some metadata to setup() call

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from distutils.core import setup
 setup(
     name='rnc2rng',
     version='1.0',
+    url='https://github.com/djc/rnc2rng',
+    author='David Mertz',
+    maintainer='Dirkjan Ochtman',
+    maintainer_email='dirkjan@ochtman.nl',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
     packages=['rnc2rng'],
     use_2to3 = True,
 )
