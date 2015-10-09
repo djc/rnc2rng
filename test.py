7911ee51e22a2c8eb4fda6e27a236ae42e5c6b38
Dirkjan Ochtman <dirkjan@ochtman.nl>
2015-10-09 22:19:34 +0200

    Start a test suite

---
+++ test.py
@@ -0,0 +1,32 @@
+from rnc2rng import rnctree
+import unittest, os
+
+SERIALIZER = rnctree.XMLSerializer()
+
+class RNCTest(unittest.TestCase):
+
+    def __init__(self, fn):
+        unittest.TestCase.__init__(self)
+        self.fn = fn
+
+    def runTest(self):
+
+        with open(self.fn) as f:
+            src = f.read()
+        with open(self.fn.replace('.rnc', '.rng')) as f:
+            expected = f.read().rstrip()
+
+        actual = SERIALIZER.toxml(rnctree.tree(src)).rstrip()
+        self.assertMultiLineEqual(expected, actual)
+
+def suite():
+    suite = unittest.TestSuite()
+    for fn in os.listdir('tests'):
+        if not fn.endswith('.rnc'):
+            continue
+        fn = os.path.join('tests', fn)
+        suite.addTest(RNCTest(fn))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
