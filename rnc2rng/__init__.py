1416c41ac4c8206010b026606453619c37cc3315
Dirkjan Ochtman <dirkjan@ochtman.nl>
2015-10-25 09:22:41 +0100

    Add simplified top-level API

---
+++ rnc2rng/__init__.py
@@ -0,0 +1,13 @@
+from . import parser, serializer
+
+def load(f):
+    return parser.parse(f)
+
+def loads(src):
+    return parser.parse(src)
+
+def dump(root, f, indent=None):
+    f.write(serializer.XMLSerializer(indent).toxml(root))
+
+def dumps(root, indent=None):
+    return serializer.XMLSerializer(indent).toxml(root)
