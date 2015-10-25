76547dcd296bf2ccc9c789c23aaa9275969dffa9
Dirkjan Ochtman <dirkjan@ochtman.nl>
2015-10-25 09:20:31 +0100

    Move XMLSerializer to a separate file

--- rnc2rng/__main__.py
+++ rnc2rng/__main__.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
-from . import rnctree
+from . import parser, serializer
 import sys
 
 def main():
 
     args = sys.argv[1:]
-    if len(args) > 0:
-        root = rnctree.tree(open(args[0]).read())
-    else:
-        root = rnctree.tree(sys.stdin.read())
-
-    xml = rnctree.XMLSerializer().toxml(root)
+    input = open(args[0]) if len(args) > 0 else sys.stdin
+    xml = serializer.XMLSerializer().toxml(parser.parse(input))
     if len(args) > 1:
         open(sys.argv[2], 'w').write(xml + '\n')
     else:
