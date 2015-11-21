d9a9dc24559caba24cde9756ebcf85171e767dca
Dirkjan Ochtman <dirkjan@ochtman.nl>
2015-11-21 17:56:29 +0100

    Use new internal parser API properly in __main__

--- rnc2rng/__main__.py
+++ rnc2rng/__main__.py
@@ -6,7 +6,7 @@ def main():
 
     args = sys.argv[1:]
     input = open(args[0]) if len(args) > 0 else sys.stdin
-    xml = serializer.XMLSerializer().toxml(parser.parse(input))
+    xml = serializer.XMLSerializer().toxml(parser.parse(f=input))
     if len(args) > 1:
         open(sys.argv[2], 'w').write(xml + '\n')
     else:
