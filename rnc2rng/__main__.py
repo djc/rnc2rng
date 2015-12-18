70fb79d58122ad6e353aad018007ccbf6c67a72a
John Vandenberg <jayvdb@gmail.com>
2015-12-19 00:41:00 +1100

    Use print function

--- rnc2rng/__main__.py
+++ rnc2rng/__main__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 from . import parser, serializer
 import sys
 
@@ -9,13 +10,13 @@ def main():
     try:
         xml = serializer.XMLSerializer().toxml(parser.parse(f=input))
     except parser.ParseError as e:
-        print 'parse error ' + e.msg
+        print('parse error ' + e.msg)
         sys.exit(1)
 
     if len(args) > 1:
         open(sys.argv[2], 'w').write(xml + '\n')
     else:
-        print xml
+        print(xml)
 
 if __name__ == '__main__':
     main()
