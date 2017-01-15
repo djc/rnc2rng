a04e50bc21681872016ae368df21c56eb29a5958
Diego Argueta <diego.argueta@cloverhealth.com>
2017-01-14 17:54:31 -0800

    Fix issue #10 and add to testing.

--- test.py
+++ test.py
@@ -1,40 +1,45 @@
+from __future__ import print_function
+
 import rnc2rng
-import unittest, os
+import unittest, glob, random
+
 
 class RNCTest(unittest.TestCase):
-
-    def __init__(self, fn):
-        unittest.TestCase.__init__(self)
-        self.fn = fn
-        self.maxDiff = None
-
-    def __str__(self):
-        return 'TestCase(%r)' % os.path.basename(self.fn)
-
     def assertBestEqual(self, expected, actual):
         if hasattr(self, 'assertMultiLineEqual'):
             self.assertMultiLineEqual(expected, actual)
         else:
             self.assertEqual(expected, actual)
 
-    def runTest(self):
+    def test_load(self):
+        """Test all RNC files in the test file directory with `load()`."""
+        for source_file in glob.iglob('tests/*.rnc'):
+            print('Testing %r...' % source_file)
+            with open(source_file) as fd:
+                root = rnc2rng.load(fd)
 
-        with open(self.fn) as f:
-            root = rnc2rng.load(f)
-        with open(self.fn.replace('.rnc', '.rng')) as f:
-            expected = f.read().rstrip()
+            with open(source_file.replace('.rnc', '.rng')) as fd:
+                expected = fd.read().strip()
+
+            actual = rnc2rng.dumps(root).strip()
+            self.assertBestEqual(expected, actual)
+
+    def test_loads(self):
+        """We should be able to parse a file without giving a filename"""
+        # Pick a random source file
+        source_file = random.choice(glob.glob('tests/*.rnc'))
+        expected_file = source_file.replace('.rnc', '.rng')
+
+        # Load the code and convert it. Everything should behave normally.
+        with open(source_file, 'r') as fd:
+            root = rnc2rng.loads(fd.read())
+
+        with open(expected_file, 'r') as fd:
+            expected = fd.read().strip()
 
         actual = rnc2rng.dumps(root).strip()
         self.assertBestEqual(expected, actual)
 
-def suite():
-    suite = unittest.TestSuite()
-    for fn in os.listdir('tests'):
-        if not fn.endswith('.rnc'):
-            continue
-        fn = os.path.join('tests', fn)
-        suite.addTest(RNCTest(fn))
-    return suite
 
 if __name__ == '__main__':
-    unittest.main(defaultTest='suite')
+    unittest.main()
