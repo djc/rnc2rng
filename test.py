3c7fa39484e736c6cd6ef9623335d16af8b1da15
Diego Argueta <diego.argueta@cloverhealth.com>
2017-01-14 18:04:19 -0800

    Factor out glob, unittest uses assertMultiLineEqual by default.

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 from __future__ import print_function
 
+import glob
+import random
+import unittest
+
 import rnc2rng
-import unittest, glob, random
+
+
+SOURCE_FILES = glob.glob('tests/*.rnc')
 
 
 class RNCTest(unittest.TestCase):
-    def assertBestEqual(self, expected, actual):
-        if hasattr(self, 'assertMultiLineEqual'):
-            self.assertMultiLineEqual(expected, actual)
-        else:
-            self.assertEqual(expected, actual)
-
     def test_load(self):
         """Test all RNC files in the test file directory with `load()`."""
-        for source_file in glob.iglob('tests/*.rnc'):
+        for source_file in SOURCE_FILES:
             print('Testing %r...' % source_file)
             with open(source_file) as fd:
                 root = rnc2rng.load(fd)
@@ -22,12 +22,12 @@ class RNCTest(unittest.TestCase):
                 expected = fd.read().strip()
 
             actual = rnc2rng.dumps(root).strip()
-            self.assertBestEqual(expected, actual)
+            self.assertEqual(expected, actual)
 
     def test_loads(self):
         """We should be able to parse a file without giving a filename"""
         # Pick a random source file
-        source_file = random.choice(glob.glob('tests/*.rnc'))
+        source_file = random.choice(SOURCE_FILES)
         expected_file = source_file.replace('.rnc', '.rng')
 
         # Load the code and convert it. Everything should behave normally.
@@ -38,7 +38,7 @@ class RNCTest(unittest.TestCase):
             expected = fd.read().strip()
 
         actual = rnc2rng.dumps(root).strip()
-        self.assertBestEqual(expected, actual)
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
