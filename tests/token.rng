djc/rnc2rng

commit 2a487b61b4f2ccf89ec43e6ee7d3c6f589bc7e93

Add external, grammar, inherit and token as keywords (see #3)

rnc2rng/parser.py
@@ -3,9 +3,9 @@ import rply, sys, os
 from codecs import BOM_UTF16_BE, BOM_UTF16_LE
 
 KEYWORDS = set([
-    'attribute', 'datatypes', 'default', 'div', 'element', 'empty', 'include',
-    'list', 'mixed', 'namespace', 'notAllowed', 'parent', 'start', 'string',
-    'text',
+    'attribute', 'datatypes', 'default', 'div', 'element', 'empty', 'external',
+    'grammar', 'include', 'inherit', 'list', 'mixed', 'namespace',
+    'notAllowed', 'parent', 'start', 'string', 'text', 'token',
 ])
 
 NCNAME = '[A-Za-z_][\w.-]*'
@@ -476,8 +476,20 @@ def id_kw_elem(s, p):
 def id_kw_empty(s, p):
     return Node('NAME', p[0].value)
 
+@pg.production('id-or-kw : EXTERNAL')
+def id_kw_external(s, p):
+    return Node('NAME', p[0].value)
+
+@pg.production('id-or-kw : GRAMMAR')
+def id_kw_grammar(s, p):
+    return Node('NAME', p[0].value)
+
 @pg.production('id-or-kw : INCLUDE')
 def id_kw_include(s, p):
+    return Node('NAME', p[0].value)
+
+@pg.production('id-or-kw : INHERIT')
+def id_kw_token(s, p):
     return Node('NAME', p[0].value)
 
 @pg.production('id-or-kw : LIST')
@@ -510,6 +522,10 @@ def id_kw_string(s, p):
 
 @pg.production('id-or-kw : TEXT')
 def id_kw_text(s, p):
+    return Node('NAME', p[0].value)
+
+@pg.production('id-or-kw : TOKEN')
+def id_kw_token(s, p):
     return Node('NAME', p[0].value)
 
 class ParseError(Exception):

commit a24478e77786a27d9cd4e28b6d384fea59379e8f

Add missing TOKEN productions
Also fix the strlit versions to emit correct rng serialization
* the <data> element does *not* contain the <value> element
  instead, <value type=>...</value> can carry type info
* add missing production for TOKEN strlit

This is partially a regression in 2a487b61b4f2ccf89ec43e6ee7d3c6f589bc7e93

rnc2rng/parser.py
@@ -438,11 +438,19 @@ def primary_string(s, p):
 
 @pg.production('primary : STRING strlit')
 def primary_typed_string(s, p):
-    return Node('DATATAG', 'string', [p[1].value])
+    return Node('LITERAL', p[1].value, [Node('ANNO_ATTR', 'type', ['string'])])
 
 @pg.production('primary : STRING LBRACE params RBRACE')
 def primary_string_parametrized(s, p):
     return Node('DATATAG', 'string', p[2])
+
+@pg.production('primary : TOKEN')
+def primary_text(s, p):
+    return Node('DATATAG', 'token')
+
+@pg.production('primary : TOKEN strlit')
+def primary_text(s, p):
+    return Node('LITERAL', p[1].value) # the default type is token
 
 @pg.production('primary : TEXT')
 def primary_text(s, p):

tests/token.rnc
@@ -1,0 +1,7 @@
+foo = element foo {
+	attribute token_abc { token "abc" },
+	attribute token { token },
+	attribute string_abc { string "abc" },
+	attribute string { string },
+	text
+}

tests/token.rng
@@ -1,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<grammar xmlns="http://relaxng.org/ns/structure/1.0"
+         datatypeLibrary="http://www.w3.org/2001/XMLSchema-datatypes">
+  <define name="foo">
+    <element>
+      <name ns="">foo</name>
+      <attribute>
+        <name ns="">token_abc</name>
+        <value>abc</value>
+      </attribute>
+      <attribute>
+        <name ns="">token</name>
+        <data type="token"/>
+      </attribute>
+      <attribute>
+        <name ns="">string_abc</name>
+        <value type="string">abc</value>
+      </attribute>
+      <attribute>
+        <name ns="">string</name>
+        <data type="string"/>
+      </attribute>
+      <text/>
+    </element>
+  </define>
+</grammar>