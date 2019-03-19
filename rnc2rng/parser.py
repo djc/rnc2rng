djc/rnc2rng

commit 214fc5271ba8a236b6ce3c5ecd0d5be467199e7b

Parenthesis grouping lowered to a different parser level

rnc2rng/parser.py
@@ -168,10 +168,6 @@ def particle_interleave_multi(s, p):
 def particle_interleave_single(s, p):
     return Node('INTERLEAVE', None, [p[0], p[2]])
 
-@pg.production('particle : BEG_PAREN pattern END_PAREN')
-def pattern_group(s, p):
-    return Node('GROUP', None, p[1])
-
 @pg.production('particle : annotated-primary MAYBE')
 def particle_maybe(s, p):
     return Node('MAYBE', None, p[0])
@@ -187,6 +183,10 @@ def particle_some(s, p):
 @pg.production('particle : annotated-primary')
 def particle_primary(s, p):
     return p[0]
+
+@pg.production('annotated-primary : BEG_PAREN pattern END_PAREN')
+def annotated_primary_group(s, p):
+    return Node('GROUP', None, p[1])
 
 @pg.production('annotated-primary : DOCUMENTATION primary')
 def annotated_primary_annotated(s, p):

commit 5e064450454476b5e58b11053a9355a11e518268

Allow annotations in front of (...) and ref (Fix #16)
Parsing a parenthesized expression should still allow annotations in front
This seems to be a regression in 214fc5271ba8a236b6ce3c5ecd0d5be467199e7b,
when the parens were lowered from particle it should have been 'primary',
not-annotated-primary, so the annotated-primary production could add to it

Serializing ref or parentRef may need to write child elements
if there are any annotations present

rnc2rng/parser.py
@@ -391,7 +391,7 @@ def particle_some(s, p):
 def particle_primary(s, p):
     return p[0]
 
-@pg.production('annotated-primary : LPAREN pattern RPAREN')
+@pg.production('primary : LPAREN pattern RPAREN')
 def annotated_primary_group(s, p):
     return Node('GROUP', None, p[1])
 

rnc2rng/serializer.py
@@ -162,7 +162,12 @@ class XMLSerializer(object):
                     self.write('<name ns="%s">%s</name>' % (ns, name))
             elif x.type in set([REF, PARENT]):
                 bits = x.type.lower(), x.name, attribs
-                self.write('<%s name="%s"%s/>' % bits)
+                if not x.value: # no parameters
+                    self.write('<%s name="%s"%s/>' % bits)
+                else:
+                    self.write('<%s name="%s"%s>' % bits)
+                    self.visit(x.value)
+                    self.write('</%s>' % x.type.lower())
             elif x.type == LITERAL:
                 bits = attribs, html.escape(x.name)
                 self.write('<value%s>%s</value>' % bits)